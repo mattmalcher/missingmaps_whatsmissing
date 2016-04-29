3337333fe3a2abb8e4c37cfd24ee79e859e1cec5
SJohnson <simonbjohnson@gmail.com>
2016-04-29 16:15:06 +0100

    Map example

---
+++ maps/liberia/js/main.js
@@ -0,0 +1,63 @@
+var baseOSM = L.tileLayer(
+        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+        attribution: '&copy; OpenStreetMap contributors'}
+);
+
+var BING_KEY = 'AuhiCJHlGzhg93IqUH_oCpl_-ZUrIE6SPftlyGYUvr9Amx5nzA-WqGcPquyFZl4L'
+var bingLayer = L.tileLayer.bing(BING_KEY);
+
+var map = L.map('map', {
+    center: [6.4281, -9.4295],
+    zoom: 8,
+    layers: [bingLayer]
+});
+
+var missingMap = L.tileLayer.wms("http://52.90.193.38/geoserver/ows", {
+    layers: 'geonode:liberia_missing_maps',
+    format: 'image/png',
+    transparent: true,
+    attribution: "BRC Maps Team",
+    styles:'liberia_missing_maps',
+}).addTo(map);
+
+var baseMaps = {
+    "OpenStreetMap": baseOSM,
+    "Bing Satellite": bingLayer
+};
+
+var overlayMaps = {
+    "Missing Maps": missingMap
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+var colors = ['#FC8D59','#7f0000'];
+var labels = ['Low Populated area missing map','High Populated area missing map'];
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend');
+            
+    div.innerHTML = "<p>Map highligting unmapped areas</p>";
+    for (var i = 0; i < labels.length; i++) {
+        div.innerHTML +='<i style="background:' + colors[i] + '"></i> ' + labels[i] + '<br />';
+    }
+
+    return div;
+    };
+
+    legend.addTo(map);
+
+function resize(){
+    $('#map').height($(window).height()-$('#header').height()-10);
+    map.invalidateSize(false);
+}
+
+$(window).load(function(){
+    resize();
+});
+$(window).resize(function(){
+    resize();
+});
--- processgeotiff.py
+++ processgeotiff.py
@@ -3,6 +3,26 @@ import gdal
 import json
 import math
 import png
+import numpy as np
+
+def array_to_raster(array,geotransform,projection):
+
+    dst_filename = 'liberia_missing_maps.tiff'
+
+    driver = gdal.GetDriverByName('GTiff')
+
+    dataset = driver.Create(
+        dst_filename,
+        len(array[0]),
+        len(array),
+        1,
+        gdal.GDT_Float32, )
+
+    dataset.SetGeoTransform(geotransform)  
+
+    dataset.SetProjection(projection)
+    dataset.GetRasterBand(1).WriteArray(array)
+    dataset.FlushCache()
 
 #Set GeoTiff driver
 driver = gdal.GetDriverByName("GTiff")
@@ -64,54 +84,54 @@ with open('liberia_buildings.geojson') as data_file:
 ######This section can be skipped for speed.  Produces osm png and worldpop png
 
 #find max building count to normalise against
-        
-maxBuildings = 0
-
-print "Finding Max building count"
-for row in osmarray:
-    for cell in row:
-        if cell>maxBuildings:
-            maxBuildings = cell
-
-print maxBuildings
-
-print "Creating osm png"
-pngarray = [[255 for i in range(cols)] for j in range(rows)]
-for row in range(0,rows):
-    for col in range(0,cols):
-        if osmarray[row][col]>0:
-            pngarray[row][col] = 255-int(math.floor(math.log(osmarray[row][col])/math.log(maxBuildings)*255))
-
-with open('ecuador_osm.png', 'wb') as png_file:
-    print len(pngarray[0])
-    print len(pngarray)
-    w = png.Writer(len(pngarray[0]), len(pngarray), greyscale=True, bitdepth=8)
-    w.write(png_file, pngarray)
-                                            
-maxPop = 0
-
-print "Finding Max building count"
-for row in rasterarray:
-    for cell in row:
-        if cell>maxPop:
-            maxPop = cell
-
-print maxPop
- 
-print "Creating world pop png"
-
-pngarray = [[255 for i in range(cols)] for j in range(rows)]
-for row in range(0,rows):
-    for col in range(0,cols):
-        if rasterarray[row][col]>1:
-            value = 255-int(math.floor(math.log(rasterarray[row][col])/math.log(maxPop)*255))
-            pngarray[row][col] = value
-
-with open('ecuador_worldpop.png', 'wb') as png_file:
-    print len(pngarray[0])
-    print len(pngarray)
-    w = png.Writer(len(pngarray[0]), len(pngarray), greyscale=True, bitdepth=8)
-    w.write(png_file, pngarray);
+##        
+##maxBuildings = 0
+##
+##print "Finding Max building count"
+##for row in osmarray:
+##    for cell in row:
+##        if cell>maxBuildings:
+##            maxBuildings = cell
+##
+##print maxBuildings
+##
+##print "Creating osm png"
+##pngarray = [[255 for i in range(cols)] for j in range(rows)]
+##for row in range(0,rows):
+##    for col in range(0,cols):
+##        if osmarray[row][col]>0:
+##            pngarray[row][col] = 255-int(math.floor(math.log(osmarray[row][col])/math.log(maxBuildings)*255))
+##
+##with open('ecuador_osm.png', 'wb') as png_file:
+##    print len(pngarray[0])
+##    print len(pngarray)
+##    w = png.Writer(len(pngarray[0]), len(pngarray), greyscale=True, bitdepth=8)
+##    w.write(png_file, pngarray)
+##                                            
+##maxPop = 0
+##
+##print "Finding Max building count"
+##for row in rasterarray:
+##    for cell in row:
+##        if cell>maxPop:
+##            maxPop = cell
+##
+##print maxPop
+## 
+##print "Creating world pop png"
+##
+##pngarray = [[255 for i in range(cols)] for j in range(rows)]
+##for row in range(0,rows):
+##    for col in range(0,cols):
+##        if rasterarray[row][col]>1:
+##            value = 255-int(math.floor(math.log(rasterarray[row][col])/math.log(maxPop)*255))
+##            pngarray[row][col] = value
+##
+##with open('ecuador_worldpop.png', 'wb') as png_file:
+##    print len(pngarray[0])
+##    print len(pngarray)
+##    w = png.Writer(len(pngarray[0]), len(pngarray), greyscale=True, bitdepth=8)
+##    w.write(png_file, pngarray);
 
 ########end of part you can skip
 
@@ -124,7 +144,7 @@ for row in range(0,rows):
         buildings = osmarray[row][col]
         if buildings == 0:
             #minimum building value
-            buildings = 0.1
+            buildings = 0.01
         
         value = rasterarray[row][col]/buildings+1
         differencearray[row][col] = value
@@ -144,15 +164,19 @@ print maxdifference
  
 print "Creating difference png"
 
-pngarray = [[255 for i in range(cols)] for j in range(rows)]
+#pngarray = [[255 for i in range(cols)] for j in range(rows)]
+pngarray = np.zeros((rows, cols))
 for row in range(0,rows):
     for col in range(0,cols):
-        if differencearray[row][col]>1:
-            value = 255-int(math.floor(math.log(differencearray[row][col])/math.log(maxdifference)*255))
+        #if differencearray[row][col]>0:
+            #value = int(math.floor(math.log(differencearray[row][col])/math.log(maxdifference)*maxdifference))
+            value = int(math.floor(differencearray[row][col]))
             pngarray[row][col] = value
 
-with open('liberia_difference_0.1_cutoff_1_no_log.png', 'wb') as png_file:
-    print len(pngarray[0])
-    print len(pngarray)
-    w = png.Writer(len(pngarray[0]), len(pngarray), greyscale=True, bitdepth=8)
-    w.write(png_file, pngarray);
+projection = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]'
+array_to_raster(pngarray,gt,projection)
+#with open('liberia_difference_0.1_cutoff_1_no_log.png', 'wb') as png_file:
+#    print len(pngarray[0])
+#    print len(pngarray)
+#    w = png.Writer(len(pngarray[0]), len(pngarray), greyscale=True, bitdepth=8)
+#    w.write(png_file, pngarray);
